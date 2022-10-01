01af1fc54ece1f0d1d93228a3585d5da7513cbf7
Geekr <geekr@hey.com>
2022-10-01 22:44:04 +0800

    将算法代码移除到独立的仓库

--- chapter04/02-class.go
+++ chapter04/02-class.go
@@ -3,30 +3,42 @@ package main
 import "fmt"
 
 type Student struct {
-	id uint
-	name string
-	male bool
-	score float64
+    id    uint
+    name  string
+    male  bool
+    score float64
 }
 
 func NewStudent(id uint, name string, score float64) *Student {
-	return &Student{id: id, name:name, score:score}
+    return &Student{id: id, name: name, score: score}
 }
 
-func (s Student) GetName() string  {
-	return s.name
+func NewStudentV2(id uint, name string, score float64) Student {
+    return Student{id: id, name: name, score: score}
+}
+
+func (s Student) GetName() string {
+    return s.name
 }
 
 func (s *Student) SetName(name string) {
-	s.name = name
+    s.name = name
 }
 
 func (s Student) String() string {
-	return fmt.Sprintf("{id: %d, name: %s, male: %t, score: %f}",
-		s.id, s.name, s.male, s.score)
+    return fmt.Sprintf("{id: %d, name: %s, male: %t, score: %f}",
+        s.id, s.name, s.male, s.score)
 }
 
 func main() {
-	student := NewStudent(1, "学院君", 100)
-	fmt.Println(student)
+    s := NewStudent(1, "学院君", 100)
+    s.SetName("学院君1号")   // ok 正常调用指针方法
+    fmt.Println(s.GetName()) // ok 调用值方法自动解引用: (*s).GetName()
+
+    s2 := NewStudentV2(2, "学院君", 90)
+    s2.SetName("学院君2号")   // ok s2 是可寻址的，所以实际调用: (&s2).SetName("学院君2号")
+    fmt.Println(s2.GetName()) // ok 正常调用值方法
+
+    NewStudent(3, "学院君", 80).SetName("学院君3号") // ok 正常调用指针方法
+    // NewStudentV2(4, "学院君", 99).SetName("学院君4号") // err 值类型调用指针方法
 }
---
+++ chapter06/02-add.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func add(a, b int) {
+    var c = a + b
+    fmt.Printf("%d + %d = %d\n", a, b, c)
+}
+
+func main() {
+    for i := 0; i < 10; i++ {
+        go add(1, i)
+    }
+    time.Sleep(1 * time.Second)
+}
